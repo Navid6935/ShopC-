79349a6d25829aaab85ee8f19ca433c3de67b876
NaviD Kian <n.soleimani@haierasa.ir>
2020-02-03 01:40:11 +0330

    Add project files.

---
+++ General/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(General.Startup))]
+namespace General
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
